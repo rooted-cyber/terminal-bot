ddbcca4e26437640db1b5fb6a738ec6a1749fb89
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-14 07:53:29 +0530

    Add files via upload

--- mbot.sh
+++ mbot.sh
@@ -2,6 +2,7 @@ fol() {
 cd /app/root/MyTGBot,/mirror-bot
 }
 dl() {
+rm -rf dow
 mkdir dow
 cd dow
 wget https://github.com/rooted-cyber/terminal-bot/raw/main/bot.zip
@@ -20,9 +21,11 @@ rm *main*
 cd help*/ext*
 rm fs*
 }
+rm -rf mirr*t
 git clone http://github.com/harshpreets63/mirror-bot
 ald
 dl
 fol
+pip install --upgrade pip
 bash req.sh
 bash start*
