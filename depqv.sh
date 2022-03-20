a9d8512d0a3647b6e02683aeac56c7b78fc95190
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-20 18:30:40 +0530

    Add files via upload

---
+++ depqv.sh
@@ -0,0 +1,16 @@
+dlc() {
+wget https://github.com/rooted-cyber/terminal-bot/raw/main/dev.zip
+wget -O .env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/env/termux%EF%B8%8F%EF%B8%8F%EF%B8%8F%EF%B8%8F%EF%B8%8F%EF%B8%8F.txt
+cp  -f dev.zip .env /root/TeamUltroid/Ultroid
+}
+dep() {
+git clone https://github.com/TeamUltroid/Ultroid
+cd Ultroid
+rm -rf .git
+dlc
+pip install --upgrade pip
+unzip dev.zip
+pip3 install -r req*
+pip3 install -r res*/st*/opt*
+}
+dep
