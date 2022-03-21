93ff75f4e7110486aec3e430acd01a6477c7edf7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-21 18:07:52 +0530

    Add files via upload

--- mbot.sh
+++ mbot.sh
@@ -1,5 +1,5 @@
 fol() {
-cd /app/root/MyTGBot,/mirror-bot
+cd /root/TeamUltroid/mirror-bot
 }
 dl() {
 rm -rf dow
@@ -7,20 +7,21 @@ mkdir dow
 cd dow
 wget https://github.com/rooted-cyber/terminal-bot/raw/main/bot.zip
 unzip bot.zip
-cp -f *init* /app/root/MyTGBot/mirr*t/bot
-cp -f *main* /app/root/MyTGBot/mirr*t/bot
-cp -f fs* /app/root/MyTGBot/mi*t/bot/help*/ext*/
-cp -f config.env /app/root/MyTGBot/m*t
-cp -f token.pickle /app/root/MyTGBot/m*t
-cp -f req.sh /app/root/MyTGBot/mi*t
+cp -f *init* /root/TeamUltroid/mirr*t/bot
+cp -f *main* /root/TeamUltroid/mirr*t/bot
+cp -f fs* /root/TeamUltroid/mi*t/bot/help*/ext*/
+cp -f config.env /root/TeamUltroid/m*t
+cp -f token.pickle /root/TeamUltroid/m*t
+cp -f req.sh /root/TeamUltroid/mi*t
 }
 ald() {
-cd /app/root/MyTGBot/mi*t/bot
+cd /root/TeamUltroid/mi*t/bot
 rm *init*
 rm *main*
 cd help*/ext*
 rm fs*
 }
+apt install aria2 -y
 rm -rf mirr*t
 git clone http://github.com/harshpreets63/mirror-bot
 ald
@@ -28,4 +29,6 @@ dl
 fol
 pip install --upgrade pip
 bash req.sh
-bash start*
+
+
+
