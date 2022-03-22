ce980145887ad85b09c8d98c9917486e24c452db
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-22 11:48:27 +0530

    Add files via upload

--- res.sh
+++ res.sh
@@ -1,15 +1,7 @@
-file() {
-mkdir deleted
-cd deleted
-for a in {1..1000};do
-touch ab
+res() {
+mkdir ab
+for a in {1..100};do
+cp RMX ab/$RANDOM$RANDOM
 done
-tar -cf ab.tar *
-du -hs ab.tar
-for b in {1..50000};do
-cp -f ab.tar $RANDOM$RANDOM$RANDOM
-done
-tar -cf abc.tar *
-du -hs abc.tar
 }
-file
+res
