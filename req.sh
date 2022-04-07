6dc019d3df5312af973d27242a1f5295284eb2ba
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-07 14:41:16 +0530

    Add files via upload

---
+++ req.sh
@@ -0,0 +1,42 @@
+rm -rf m*t
+git clone http://github.com/harshpreets63/mirror-bot
+cd m*t
+sudo apt install aria2c -y
+sudo apt install unzip -y
+wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
+wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
+sudo chmod -R 777 *
+sudo pip install --upgrade pip
+sudo pip install -r req*
+sudo pip install anytree
+sudo pip install gunicorn
+sudo pip install urllib3
+sudo pip install yt_dlp
+sudo pip install torrentool==1.1.0
+sudo pip install qbittorrent-api
+sudo pip install TgCrypto
+sudo pip install telegraph
+sudo pip install tenacity
+sudo pip install requests
+sudo pip install python-telegram-bot
+sudo pip install python-magic
+sudo pip install python-dotenv
+sudo pip install Pyrogram
+sudo pip install cfscrape
+sudo pip install speedtest-cli
+sudo pip install filesplit
+sudo pip install appdirs
+sudo pip install aria2p
+sudo pip install psycopg2
+sudo pip install beautifulsoup4
+sudo pip install google-api-python-client
+sudo pip install google-auth-httplib2
+sudo pip install google-auth-oauthlib
+sudo pip install js2py
+sudo pip install lxml
+sudo pip install lk21
+sudo pip install megasdkrestclient
+sudo pip install pybase64
+sudo pip install psutil
+sudo pip install pillow
+sudo pip install hachoir
