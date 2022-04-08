ee410c413969caedcce76da6f1c821fafabd3109
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-08 16:49:21 +0530

    Add files via upload

---
+++ reqan.sh
@@ -0,0 +1,53 @@
+rin() {
+sudo pip install --upgrade sudo pip
+sudo pip install anytree
+sudo pip install appdirs
+sudo pip install aria2p
+sudo pip install attrdict
+sudo pip install beautifulsoup4
+sudo pip install bencoding
+sudo pip install cfscrape
+sudo pip install feedparser
+sudo pip install flask
+sudo pip install google-api-python-client
+sudo pip install google-auth-httplib2
+sudo pip install google-auth-oauthlib
+sudo pip install gunicorn
+sudo pip install lk21
+sudo pip install lxml
+sudo pip install pillow
+sudo pip install psutil
+sudo pip install psycopg2-binary
+sudo pip install pybase64
+sudo pip install pyrogram
+sudo pip install python-dotenv
+sudo pip install python-magic
+sudo pip install python-telegram-bot
+sudo pip install qbittorrent-api
+sudo pip install requests
+sudo pip install speedtest-cli
+sudo pip install six
+sudo pip install telegraph
+sudo pip install tenacity
+sudo pip install tgCrypto
+sudo pip install urllib3
+sudo pip install yt-dlp
+sudo pip install google-api-python-client
+sudo pip install progress
+sudo pip install progressbar2
+sudo pip install httplib2shim
+sudo pip install google_auth_oauthlib
+sudo pip install pyrogram
+}
+dl() {
+sudo apt install aria2 -y
+rm -rf m*t*t
+git clone https://github.com/anasty17/mirror-leech-telegram-bot
+cd m*t*t
+sudo wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
+sudo wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
+sudo rin
+sudo chmod -R 777 *
+sudo chmod -R 777 .*
+}
+sudo dl
