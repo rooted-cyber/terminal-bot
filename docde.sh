edd0f9aa44286209ee50a892bdcaab9f741a0dc8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-21 20:52:35 +0530

    Add files via upload

--- docde.sh
+++ docde.sh
@@ -1,12 +1,22 @@
 a() {
+clear
+printf "\n\n\033[1;92m Updating package....\n"
+sleep 0.50
 apt update -y
 apt upgrade -y
 apt install wget -y
 apt install git -y
+clear
+printf "\n\n Cloning Ultroid\n"
 git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
+printf "\n\n Installing Requirements\n"
 pip3 install -r requ*
+pip3 install speedtest-cli
 pip3 install -r res*/st*/opt*
 wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/.env
+clear
+printf "\n Successfully installed ultroid \n\n"
+sleep 0.50
 }
 a
