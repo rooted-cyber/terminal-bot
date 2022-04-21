f47a5cfbf996e89e3f55a4407e655f9a4d3498e3
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-21 19:18:50 +0530

    Add files via upload

---
+++ docde.sh
@@ -0,0 +1,12 @@
+a() {
+apt update -y
+apt upgrade -y
+apt install wget -y
+apt install git -y
+git clone https://github.com/TeamUltroid/Ultroid
+cd Ultroid
+pip3 install -r requ*
+pip3 install -r res*/st*/opt*
+wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/.env
+}
+a
