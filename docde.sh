9314d9bab2fa7f72a7d2b78df8ec5ba71a32b582
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-22 19:14:12 +0530

    Add files via upload

--- docde.sh
+++ docde.sh
@@ -24,31 +24,32 @@ a() {
 clear
 printf "\n\n\033[1;92m Updating package....\n"
 sleep 0.50
-apt update -y &> /dev/null & spin
-apt upgrade -y &> /dev/null & spin
+apt update -y
+apt upgrade -y
 printf "\n\n\033[1;93m Installing wget\n"
-apt install wget -y &> /dev/null & spin
+apt install wget -y
 printf "\n\n\033[1;94m Installing git\n"
-apt install git -y &> /dev/null & spin
+apt install git -y
 printf "\n\n \033[1;94m Cloning Ultroid\n"
-git clone https://github.com/TeamUltroid/Ultroid &> /dev/null & spin
+git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
 printf "\n\n\033[1;93m Installing ultroid Requirements\n"
-pip3 install -r requ* &> /dev/null & spin
+pip3 install -r requ*
 printf "\n\n\033[1;93m Installing Optional requirements \n"
-pip3 install -r res*/st*/opt* &> /dev/null & spin
+pip3 install -r res*/st*/opt*
 printf "\n\n\033[1;93m Installing speedtest\n"
-pip3 install speedtest-cli &> /dev/null & spin
+pip3 install speedtest-cli 
+pip3 install --no-cache-dir -r requirements.txt
 
 printf "\n\n\033[1;93m Installing Ultroid .env\n"
-wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/.env &> /dev/null & spin
+wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/.env
 clear
 printf "\n \033[1;92m Successfully installed ultroid \n\n"
 sleep 0.50
 printf "\n\n\033[1;93m Installing mirror-bot requirement\n"
 cd ..
-wget https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/master/requirements.txt > /dev/null 2>&1
-pip3 install --no-cache-dir -r requirements.txt &> /dev/null & spin
+wget https://raw.githubusercontent.com/anasty17/mirror-leech-telegram-bot/master/requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
 
 }
 a
