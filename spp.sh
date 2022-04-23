e8493b32b547f1243b33930c106a1dc6c7544741
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-23 12:03:16 +0530

    Add files via upload

---
+++ spp.sh
@@ -0,0 +1,28 @@
+a() {
+clear
+printf "\n\n\033[1;92m Updating package....\n"
+sleep 0.50
+apt update -y
+apt upgrade -y
+printf "\n\n\033[1;93m Installing wget\n"
+apt install wget -y
+printf "\n\n\033[1;94m Installing git\n"
+apt install git -y
+printf "\n\n \033[1;94m Cloning Ultroid\n"
+git clone https://github.com/TeamUltroid/Ultroid
+cd Ultroid
+printf "\n\n\033[1;93m Installing ultroid Requirements\n"
+pip3 install -r requ*
+printf "\n\n\033[1;93m Installing Optional requirements \n"
+pip3 install -r res*/st*/opt*
+printf "\n\n\033[1;93m Installing speedtest\n"
+pip3 install speedtest-cli
+printf "\n\n\033[1;93m Installing Ultroid .env\n"
+wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/.env
+clear
+bash startup
+printf "\n \033[1;92m Successfully installed ultroid \n\n"
+sleep 0.50
+
+}
+a
