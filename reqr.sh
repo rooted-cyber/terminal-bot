d4a8fe04466b8146e4545381c337a3f5f4dd6520
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-23 19:58:00 +0530

    Add files via upload

---
+++ reqr.sh
@@ -0,0 +1,66 @@
+rin() {
+pip install --upgrade pip
+pip install anytree
+pip install appdirs
+pip install aria2p
+pip install attrdict
+pip install beautifulsoup4
+pip install bencoding
+pip install cfscrape
+pip install feedparser
+pip install flask
+pip install google-api-python-client
+pip install google-auth-httplib2
+pip install google-auth-oauthlib
+pip install gunicorn
+pip install lk21
+pip install lxml
+pip install pillow
+pip install psutil
+pip install psycopg2-binary
+pip install pybase64
+pip install pyrogram
+pip install python-dotenv
+pip install python-magic
+pip install python-telegram-bot
+pip install qbittorrent-api
+pip install requests
+pip install speedtest-cli
+pip install six
+pip install telegraph
+pip install tenacity
+pip install tgCrypto
+pip install urllib3
+pip install yt-dlp
+pip install google-api-python-client
+pip install progress
+pip install progressbar2
+pip install httplib2shim
+pip install google_auth_oauthlib
+pip install pyrogram
+}
+dl() {
+apt install aria2 -y
+apt install wget -y
+rm -rf m*t*t
+git clone https://github.com/anasty17/mirror-leech-telegram-bot
+cd m*t*t
+pwd
+pwd
+pwd
+pwd
+wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cn
+wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
+rin
+pwd
+pwd
+pwd
+pwd
+ls
+ls
+ls -a
+ls -a
+cd m*t*t
+printf "\033[1;92m";bash start*
+}
+dl
