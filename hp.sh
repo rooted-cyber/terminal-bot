88cd436442f04bf3ea2173584ea9e06427645ac8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-04-24 17:44:47 +0530

    Add files via upload

---
+++ hp.sh
@@ -0,0 +1,31 @@
+upd() {
+random
+echo "Updating bot"
+git add .
+git add -f token.pickle config.env drive_folder
+git commit -m "changes"
+git push heroku
+}
+cre() {
+echo "hj" >> drive_folder
+appname="botmirrormaruf$RANDOM"
+region="eu"
+heroku create --region $region $appname
+heroku git:remote -a $appname
+heroku stack:set container -a $appname
+heroku apps:destroy --confirm $appname
+heroku create --region $region $appname
+heroku git:remote -a $appname
+heroku stack:set container -a $appname
+git add .
+git add -f token.pickle config.env drive_folder
+git commit -m "changes"
+git push heroku
+echo "hi" >> bot123
+
+}
+if [ -e bot123 ];then
+upd
+else
+cre
+fi
