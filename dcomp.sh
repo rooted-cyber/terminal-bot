4cfbde686789eed4b1083912d68c345781b77289
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-09 20:55:38 +0530

    Add files via upload

---
+++ dcomp.sh
@@ -0,0 +1,16 @@
+dl() {
+rm start.py
+wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/start.py
+cd help*
+rm config.py
+wget https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/config.py
+bash run.sh
+}
+star() {
+git clone https://github.com/1Danish-00/CompressorBot
+cd Com*
+pip install --upgrade pip
+pip install -r req*
+dl
+}
+star
