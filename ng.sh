e7dd5da0638f2d001cd9fd328622d34e0cf66f1b
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-25 06:17:30 +0530

    Add files via upload

---
+++ ng.sh
@@ -0,0 +1,5 @@
+php -S localhost:3333 > /dev/null 2>&1 &
+./ngrok http 3333 > /dev/null 2>&1 &
+sleep 12
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[-0-9a-z]*\.ngrok.io")
+printf " Your Ngrok link: %s\n\n" $link
